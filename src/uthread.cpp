cdc8130f4f2a027b966275c140ab343ffdd54616
Kaal-09 <sakshamgautam369@gmail.com>
2025-12-27 12:37:56 +0530

    basic functions completed and working

--- src/uthread.cpp
+++ src/uthread.cpp
@@ -1,21 +1,44 @@
 #include "uthread/uthread.h"
 #include <iostream>
+#include "scheduler.h"
+#include <vector>
+
+static Schedular schedular;
+static std::vector <TCB> tcbs;
+static std::uint64_t next_id = 1;
 
 namespace uthread {
     void init() {
-        std::cout << "[uthread] init called\n";
+        tcbs.reserve(16);
+        
+        TCB main_thread;
+        main_thread.id = 0;
+        main_thread.state = ThreadState::RUNNING;
+        main_thread.func = nullptr;
+        main_thread.arg = nullptr;
+
+        tcbs.push_back(main_thread);
+        schedular.add_thread(&tcbs[0]);
+        schedular.init();
     }
+
     thread_t create(void (*func)(void*), void* arg) {
-        std::cout << "[uthread] create called\n";
-        // TEMPORARY: just call the function directly
-        func(arg);
-        return 0;
+        TCB t;
+        t.id = next_id++;
+        t.state = ThreadState::READY;
+        t.func = func;
+        t.arg = arg;
+
+        tcbs.push_back(t);
+        schedular.add_thread(&tcbs.back());
+
+        return t.id;
     }
     void yield() {
-        std::cout << "[uthread] yield called\n";
+        schedular.yield();
     }
 
     void exit() {
-        std::cout << "[uthread] exit called\n";
+        schedular.exit_current();
     }
 }
